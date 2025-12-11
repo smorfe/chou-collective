392f292c59e304799d2ca3f8d621d4ea93f232ef
shielamorfeee <shiela.morfe@yahoo.ca>
2025-12-11 01:24:00 -0800

    add: head

--- src/pages/index.js
+++ src/pages/index.js
@@ -190,3 +190,12 @@ const TemplateHomepage = () => {
 };
 
 export default TemplateHomepage;
+
+export const Head = () => {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>CHOU Collective - Test</title>
+    </>
+  )
+}
